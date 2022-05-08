5e64ac72b725cd183d2dd58320dc907ed0e122cf
ArabboyKayumov <arabboykayumov@gmail.com>
2022-05-08 20:08:30 +0300

    table created

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,6 +1,32 @@
 import React from "react";
+import { data } from "../../mock/mock";
 
 export const Table = () => {
-  return <div>Table</div>;
+  return (
+    <div>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((value) => (
+            <tr key={value.id}>
+              <td>{value.id}</td>
+              <td>{value.name}</td>
+              <td>{value.email}</td>
+              <td>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 export default Table;
